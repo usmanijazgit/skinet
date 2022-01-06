9a2951048139b0ccff916877e50bd7aae2805c0e
Usman Ijaz <usman.ijaz@city.ac.uk>
2022-01-06 11:14:31 +0000

    Initial Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+  public class StoreContext : DbContext
+  {
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+  }
+
+}
