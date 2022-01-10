7c7e467d275340f47220bd00d2a586417738472e
Usman Ijaz <usman.ijaz@city.ac.uk>
2022-01-10 13:03:32 +0000

    Error Handeling

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using API.DTOs;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -10,9 +11,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class ProductsController : ControllerBase
+  public class ProductsController : BaseApiController
   {
 
     private readonly iGenericRepository<Product> _productsRepo;
@@ -41,10 +40,14 @@ namespace API.Controllers
     }
 
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ProductToReturnDTO>> GetProduct(int id)
     {
       var spec = new ProductsWithTypesAndBrandsSpecifications(id);
       var product = await _productsRepo.GetEntityWithSpec(spec);
+
+      if(product == null) return NotFound(new ApiResponse(404));
 
       return _mapper.Map<Product, ProductToReturnDTO>(product);
     }
---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,40 @@
+using API.Errors;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+            
+            services.AddScoped<iProductRepository, ProductRepository>();
+            services.AddScoped(typeof(iGenericRepository<>), (typeof(GenericRepository<>)));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+            options.InvalidModelStateResponseFactory = actionContext => 
+            {
+                var errors = actionContext.ModelState
+                .Where(e => e.Value.Errors.Count > 0)
+                .SelectMany(x => x.Value.Errors)
+                .Select(x => x.ErrorMessage).ToArray();
+
+                var errorResponse = new ApiValidationErrorResponse 
+                {
+                Errors = errors
+                };
+
+                return new BadRequestObjectResult(errorResponse);
+                
+            };
+        });
+
+        return services;
+
+        }
+    }
+}
