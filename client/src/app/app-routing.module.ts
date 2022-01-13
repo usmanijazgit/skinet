94541ffdb2528c81fec62b91894d6833a2cf05e9
Usman Ijaz <usman.ijaz@city.ac.uk>
2022-01-13 16:53:21 +0000

    revert model

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
- {path: '', component: HomeComponent},
- {path: 'test-error', component: TestErrorComponent},
- {path: 'server-error', component: ServerErrorComponent},
- {path: 'not-found', component: NotFoundComponent},
- {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+ {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
+ {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+ {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
+ {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
+ {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
  {path: '**', redirectTo: '', pathMatch: 'full'}
 
 ];
